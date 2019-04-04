62150599f130b42d00e730833cf3bc8001572d53
tushar-1728 <tushar-1728@github.com>
2019-04-04 20:23:40 +0530

    added client file

--- memory_manager/Readme.md
+++ memory_manager/Readme.md
@@ -1,2 +1,2 @@
 In this memory manager I have taken memory from system using malloc function and then
-I have maintained book-keeping for that.
+I have maintained book-keeping for the memory that has been allocated.
---
+++ memory_manager/client.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "a.h"
+
+int main(){
+	// printf("size of: %d %d\n", sizeof(Header), sizeof(unsigned));
+	allocate(265);
+	void *a = mymalloc(50);
+	void *b = mymalloc(30);
+	void *c = mymalloc(10);
+	void *d = mymalloc(20);
+	void *e = mymalloc(75);
+	display_mem_map();
+	myfree(d);
+	display_mem_map();
+	myfree(e);
+	display_mem_map();
+	// myfree(c);
+	// display_mem_map();
+	// mymalloc(25);
+	// display_mem_map();
+	free(p);
+	return 0;
+}
