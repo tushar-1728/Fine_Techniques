f0527de0838aeca7fcb698e837af2243aca5cd55
Tushar Raj <43772524+tushar-1728@users.noreply.github.com>
2019-05-27 11:18:13 +0530

    Update bitwise_sort.c

--- Bitwise sorting/bitwise_sort.c
+++ Bitwise sorting/bitwise_sort.c
@@ -10,18 +10,23 @@ int main(){
 	int arr_limit;
 	printf("Enter no of elements: ");
 	scanf("%d", &n);
-	int *arr = (int*)calloc(n, sizeof(int));
-	printf("Enter the greatest integer among all the elements: ");
+	// taking input from user the maximum limit of numbers to be sorted
+	// and then calculating the size of the array
+	printf("Enter the greatest integer among all the elements(exclusive): ");
 	scanf("%d", &maxlimit);
 	arr_limit = ceil(maxlimit / 32.00);
-	printf("%d\n", arr_limit);
+	// allocating the array
+	int *arr = (int*)calloc(arr_limit, sizeof(int));
+	// taking input of numbers to be sorted
+	// and then making the corresponding bit 1
 	printf("Enter the elements: ");
 	for(int i = 0; i < n; ++i){
 		scanf("%d", &inp);
-		arr_no = inp / 32;
-		bit_no = inp % 32;
-		arr[arr_no] |= (1 << bit_no);
+		arr_no = inp / 32;  // dividing by 32 decides bit of which array element will be modified
+		bit_no = inp % 32;	// modulo by 32 decides which bit of selected array element
+		arr[arr_no] |= (1 << bit_no);  // setting the bit 1
 	}
+	// traversing through the bits to get sorted elements
 	for(int i = 0; i < arr_limit; ++i){
 		for(int j = 0; j < 32; ++j){
 			if(arr[i] & (1 << j)){
@@ -29,4 +34,5 @@ int main(){
 			}
 		}
 	}
+	printf("\n");
 }
